33ae0979fd10726085cc996f6a99d96bd5c5f0b0
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-08-24 22:52:13 -0300

    ASP.NET

---
+++ Fiap.Aula03/Fiap.Aula03.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula03.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Estoque()
+        {
+            return View();
+        }
+
+    }
+}
