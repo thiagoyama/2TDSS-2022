2c0383aa3c188be904ca2f9dae1b46dedae18179
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-08-26 20:57:36 -0300

    Controllers

--- Fiap.Aula03/Fiap.Aula03.Web/Controllers/ProdutoController.cs
+++ Fiap.Aula03/Fiap.Aula03.Web/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap.Aula03.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiap.Aula03.Web.Controllers
 {
@@ -9,9 +10,21 @@ namespace Fiap.Aula03.Web.Controllers
             return View();
         }
 
+        [HttpGet] //Retornar a página com o formulário
         public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost] //Receber os parâmetros do formulário
+        public IActionResult Cadastrar(Produto produto)
+        {
+            //Enviar a descrição do produto para a página
+            ViewData["desc"] = $"Descrição: {produto.Descricao}";
+            //Enviar o objeto produto para a view
+            ViewBag.prod = produto;
+            return View("Sucesso");
+            //return Content($"Descrição: {produto.Descricao}, Valor: {produto.Valor}");
         }
 
         public IActionResult Estoque()
