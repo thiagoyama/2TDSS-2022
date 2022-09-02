cf4d32a093be326dd05e7f87c1af07f2966870d2
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-09-02 20:57:44 -0300

    Razor

---
+++ Fiap.Aula04/Fiap.Aula04.Web/Controllers/CarroController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Aula04.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula04.Web.Controllers
+{
+    public class CarroController : Controller
+    {
+        //Lista de carro para simular o banco de dados
+        private static List<Carro> _lista = new List<Carro>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] //Abrir a página com o formulário HTML
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Carro carro)
+        {
+            //Adicionar o carro na lista
+            _lista.Add(carro);
+            //Mandar uma mensagem de sucesso para a view
+            TempData["msg"] = "Carro cadastrado!";
+            return View();
+        }
+
+    }
+}
---
+++ Fiap.Aula04/Fiap.Aula04.Web/Models/Carro.cs
@@ -0,0 +1,11 @@
+﻿namespace Fiap.Aula04.Web.Models
+{
+    public class Carro
+    {
+        public string? Modelo { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public string? Tipo { get; set; }
+        public bool TetoSolar { get; set; }
+    }
+}
