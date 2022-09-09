3d5edf213912b1c5ed9bd5a1b96dde6e84813893
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-09-09 20:56:20 -0300

    Listar e Atualizar

--- Fiap.Aula04/Fiap.Aula04.Web/Controllers/CarroController.cs
+++ Fiap.Aula04/Fiap.Aula04.Web/Controllers/CarroController.cs
@@ -1,5 +1,6 @@
 ﻿using Fiap.Aula04.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Fiap.Aula04.Web.Controllers
 {
@@ -7,27 +8,52 @@ namespace Fiap.Aula04.Web.Controllers
     {
         //Lista de carro para simular o banco de dados
         private static List<Carro> _lista = new List<Carro>();
+        private static int _id = 0; //Controla o ID
+
+        [HttpGet] //Abrir o formulário com os dados preenchidos
+        public IActionResult Editar(int id)
+        {
+            //Recuperar a posição do carro na lista através do id
+            var index = _lista.FindIndex(c => c.Codigo == id);
+            //Recuperar o carro através do ID
+            var carro = _lista[index];
+            //Enviar o carro para a view
+            return View(carro);
+        }
 
         public IActionResult Index()
         {
-            return View();
+            //Enviar a lista de carro para a view
+            return View(_lista);
         }
 
         [HttpGet] //Abrir a página com o formulário HTML
         public IActionResult Cadastrar()
         {
+            CarregarMarcas();
             return View();
+        }
+
+        private void CarregarMarcas()
+        {
+            //Criar uma lista de string com as marcas
+            var lista = new List<string>(new string[] {"FIAT", "Hyundai",
+                "Ford", "Volkswagen", "Caoa"});
+            //Envia as opções do select da marca para a view
+            ViewBag.marcas = new SelectList(lista);
         }
 
         [HttpPost]
         public IActionResult Cadastrar(Carro carro)
         {
+            //Setar o código do carro
+            carro.Codigo = ++_id;
             //Adicionar o carro na lista
             _lista.Add(carro);
             //Mandar uma mensagem de sucesso para a view
             TempData["msg"] = "Carro cadastrado!";
-            return View();
+            //Redireciona para o método Cadastrar
+            return RedirectToAction("Cadastrar");
         }
-
     }
 }
--- Fiap.Aula04/Fiap.Aula04.Web/Models/Carro.cs
+++ Fiap.Aula04/Fiap.Aula04.Web/Models/Carro.cs
@@ -1,11 +1,26 @@
-﻿namespace Fiap.Aula04.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Aula04.Web.Models
 {
     public class Carro
     {
+        public int Codigo { get; set; }
+
         public string? Modelo { get; set; }
         public int Ano { get; set; }
         public decimal Valor { get; set; }
         public string? Tipo { get; set; }
+
+        public string? Marca { get; set; }
+
+        [Display(Name = "Possui teto solar?")]
         public bool TetoSolar { get; set; }
+        public Combustivel? Combustivel { get; set; }
     }
+
+    public enum Combustivel
+    {
+        Etanol, Gasolina, Flex, Eletrico, Diesel
+    }
+
 }
