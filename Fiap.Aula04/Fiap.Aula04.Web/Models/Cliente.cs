19d2db10d315ec0fcac5a8d3a0ff986b9650b25b
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-09-16 20:55:36 -0300

    Criando Tag Helpers e Exercício

---
+++ Fiap.Aula04/Fiap.Aula04.Web/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace Fiap.Aula04.Web.Models
+{
+    public class Cliente
+    {
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Fidelizado { get; set; }
+        public Genero Genero { get; set; }
+
+        //Select com as opções: Fundamental, Médio, Superior
+        public string? Formacao { get; set; }
+    }
+
+    public enum Genero
+    {
+        Feminino, Masculino, Outros
+    }
+}
---
+++ Fiap.Aula04/Fiap.Aula04.Web/TagHelpers/BotaoTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Fiap.Aula04.Web.TagHelpers
+{
+    //Tag: <botao></botao>
+    //Tag resultado: <button class="btn btn-primary">Cadastrar</button>
+    public class BotaoTagHelper : TagHelper
+    {
+        public string? Texto { get; set; } = "Cadastrar";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //Definir o nome da tag html
+            output.TagName = "button";
+            //Definir os atributos da tag html
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+            //Definir o conteúdo da tag html
+            output.Content.SetContent(Texto);
+        }
+    }
+}
