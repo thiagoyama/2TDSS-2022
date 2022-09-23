355696646031ef8c48f3c527af25507cac90920c
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-09-23 20:57:14 -0300

    Entity Framework

---
+++ Fiap.Aula05/Fiap.Aula05.Web/Controllers/VooController.cs
@@ -0,0 +1,21 @@
+﻿using Fiap.Aula05.Web.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula05.Web.Controllers
+{
+    public class VooController : Controller
+    {
+        private GestaoAereaContext _context;
+
+        //O contexto é injetado no construtor
+        public VooController(GestaoAereaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
